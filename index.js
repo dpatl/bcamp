3895af1e62c2454da967e37f07f9deb78e0eb007
kendall-wong <kendallwong1@gmail.com>
2019-04-13 21:18:29 -0400

    added safe buffer module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var _ = require('underscore');
 var ids = [];
+var x = false; 
 request("http://api.reimaginebanking.com/accounts?key=ea28bcabdb8040d803667622d9defbe6", function(error, response, body) {
 	//convert data into usable array of json objects
 	var json = JSON.parse(body);
@@ -8,8 +9,13 @@ request("http://api.reimaginebanking.com/accounts?key=ea28bcabdb8040d803667622d9
 		ids.push(json[idx]._id);
 	}
 	console.log(ids);
+	x = true;
 	console.log('---------------------------------');
 })
+
+console.log(x);
+console.log(ids);
+
 /*
 console.log(ids);
 for (var i =0; i < ids.length; i++) {
