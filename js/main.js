9d6ca53a0474a76b8a713b64197404346bc2e7e7
dpatl <dpatel17@umd.edu>
2019-04-13 22:25:48 -0400

    navigation between pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+
+$(function() {
+  var ele = $('#counter');
+  var clr = null;
+  var rand = (Math.random() * 100000) >> 0;
+  (loop = function() {
+    clearTimeout(clr);
+    (inloop = function() {
+      ele.html(rand+=1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      if(!(rand % 50)) {
+        return;
+      }
+      clr = setTimeout(inloop, 100);
+    })(); 
+    setTimeout(loop, (Math.random() * 100));
+  })();
+});
+
+
+
+
 (function ($) {
     "use strict";
 
