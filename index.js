56d8c8ea755f6e07e8bba96d3f62e4ca20af1569
dpatl <dpatel17@umd.edu>
2019-04-14 04:33:50 -0400

    yelp

--- index.js
+++ index.js
@@ -6,21 +6,62 @@ const yelp = require('yelp-fusion');
 // from https://www.yelp.com/developers/v3/manage_app
 const apiKey = 'iWc4aVeCqx_MaWiIIkxqw2oIcb7eNpHUx-_8fMggFg0q_MszgAnESqXjuaNhRtSVs56WxFrh6svdWvJ3asHrtnaatYxxtatnby_e6kXsas64dz_GJVA1wI1VK9CyXHYx';
 
-const searchRequest = {
-  name:'Chipotle Mexican Grill',
-  address: 'South Meadow Street, Ithica, NY'
-};
+var merch = {
+    "_id": "57cf75cea73e494d8675ec4a",
+    "name": "Pastimes Antiques",
+    "category": [
+      "furniture_store",
+      "home_goods_store",
+      "store",
+      "point_of_interest",
+      "establishment"
+    ],
+    "geocode": {
+      "lat": 35.909,
+      "lng": -79.046
+    },
+    "address": {
+      "street_number": "hellooo"
+    }
+  }
 
-const client = yelp.client(apiKey);
+business(merch);
+function business(merchant) {
+	var name = merchant.name;
+	var street = merchant.street_name;
+	var city = merchant.city;
+	var state = merchant.state;
+	console.log(name);
+	console.log("--------------------------------");
+	const searchRequest = {
+		name: name,
+		address: street + ', ' + city + ', ' + state
+	}
 
-client.businessMatch({
-  name: 'Chipotle Mexican Grill',
-  address1: 'South Meadow Street',
-  city: 'Ithica',
-  state: 'NY',
-  country: 'US'
-}).then(response => {
-  console.log(response.jsonBody.businesses[0]);
-}).catch(e => {
-  console.log(e);
-});
+	const client = yelp.client(apiKey);
+
+	client.businessMatch({
+	  name: name,
+	  address1: street,
+	  city: city,
+	  state: state,
+	  country: 'US'
+	}).then(response => {
+	  var a = response.jsonBody.businesses[0].id;
+	  client.business(a).then(response => {
+	  var bod = response.jsonBody;
+	  console.log(response.jsonBody);
+	  return bod;
+	}).catch(e => {
+	  console.log(e);
+	});
+	}).catch(e => {
+		console.log(e);
+	});
+
+	return bod;
+
+}
+
+
+ 
