fa2809b7830d382a3086c4572ae139143f147304
dpatl <dpatel17@umd.edu>
2019-04-13 21:06:29 -0400

    everything

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var request = require('request');
 var _ = require('underscore');
-
-request('http://api.reimaginebanking.com/accounts?key=a3555d774ac75bee7c1c13c843980407', function(error, response, body) {
+var ids = [];
+request("http://api.reimaginebanking.com/accounts?key=ea28bcabdb8040d803667622d9defbe6", function(error, response, body) {
 	//convert data into usable array of json objects
 	var json = JSON.parse(body);
 	for(var idx in json){
-		console.log(json[idx]);
+		ids.push(json[idx]._id);
 	}
-
+	console.log(ids);
+	console.log('---------------------------------');
 })
+/*
+console.log(ids);
+for (var i =0; i < ids.length; i++) {
+	console.log("GGDGDGDGGD");
+
+		url = "http://api.reimaginebanking.com/accounts/" + ids[i]+ "/purchases?key=ea28bcabdb8040d803667622d9defbe6";
+		console.log(url);
+		request(url, 
+			function(error, response, body) {
+			var json = JSON.parse(body);
+			for(var idx in json){
+				console.log(json[idx]);
+			}
+		})
+}
+*/
